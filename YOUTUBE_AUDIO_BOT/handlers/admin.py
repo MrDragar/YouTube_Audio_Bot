48d9112abb50308a894d1484bcf9fea0c558351b
MrDragar <yshhenyaev@mail.ru>
2022-06-22 17:10:44 +0400

    fix send_post

--- YOUTUBE_AUDIO_BOT/handlers/admin.py
+++ YOUTUBE_AUDIO_BOT/handlers/admin.py
@@ -1,5 +1,6 @@
 from aiogram.types import Message
 from aiogram.dispatcher import FSMContext
+from aiogram.utils.exceptions import BotBlocked, UserDeactivated
 
 from YOUTUBE_AUDIO_BOT.database import get_all_id
 from YOUTUBE_AUDIO_BOT.states import PostingData
@@ -14,6 +15,12 @@ async def set_post(message: Message):
 async def send_post(message: Message, state: FSMContext):
     users = get_all_id()
     for user_id in users:
-        await bot.send_message(user_id, message.text)
+        try:
+            await bot.send_message(user_id, message.text)
+        except BotBlocked:
+            pass
+        except UserDeactivated:
+            pass
+
     await state.finish()
 
