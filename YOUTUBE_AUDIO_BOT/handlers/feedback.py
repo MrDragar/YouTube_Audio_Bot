7beb0947c2faadb316ee24bd83e4c42c5f96a6a5
MrDragar <yshhenyaev@mail.ru>
2022-12-26 12:53:52 +0300

    Get user's name in feedback

--- YOUTUBE_AUDIO_BOT/handlers/feedback.py
+++ YOUTUBE_AUDIO_BOT/handlers/feedback.py
@@ -14,5 +14,5 @@ async def set_feedback(message: Message, language: str):
 async def send_feedback(message: Message, state: FSMContext):
     text = message.text
     for user_id in admins_id:
-        await bot.send_message(user_id, text + "/n От " + message.from_user.url)
+        await bot.send_message(user_id, text + "/n От " + f"{message.from_user.username} {message.from_user.full_name}")
     await state.finish()
