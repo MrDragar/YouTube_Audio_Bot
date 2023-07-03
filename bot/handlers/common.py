17bd001e7343d9ebe255bd0648d8798b5e984e3f
MrDragar <83676864+MrDragar@users.noreply.github.com>
2023-07-03 23:31:17 +0300

    Add localization

--- bot/handlers/common.py
+++ bot/handlers/common.py
@@ -5,6 +5,8 @@ from aiogram.handlers import MessageHandler
 from aiogram.methods import SendMessage
 from aiogram.filters import Command, Text
 from aiogram import types
+from aiogram.utils.i18n import lazy_gettext as __, gettext as _
+from aiogram import F
 
 from . base_handlers import StateMassageHandler
 from bot.states import Youtube
@@ -13,25 +15,23 @@ common_router = Router()
 
 
 @common_router.message(Command("cancel"))
-@common_router.message(Text("отмена", ignore_case=True))
-@common_router.message(Text("cancel", ignore_case=True))
-@common_router.message(Text("вiдмiна", ignore_case=True))
+@common_router.message(F.text == __("Отмена"))
+@common_router.message(F.text.lower() == __("отмена"))
 class CancelHandler(StateMassageHandler):
-    text = "Отмена"
-
     async def handle(self) -> Any:
-        await SendMessage(chat_id=self.chat.id, text=self.text,
+        await SendMessage(chat_id=self.chat.id, text=_("Отмена"),
                           reply_markup=types.ReplyKeyboardRemove())
         await self.state.clear()
 
 
 @common_router.message(Command("start", "help"))
 class StartHandler(StateMassageHandler):
-    text = "Привет. С помощью этого бота ты можешь скачать любое видео или аудио с Ютуба. " \
-           "Для этого вам необходимо скинуть ссылку на этот ролик. По всем вопросам пишите на " \
-           "yshhenyaev@mail.ru\n" \
-           "Для смены языка пропишите \n/language ."
-
     async def handle(self) -> Any:
         await self.state.set_state(Youtube.type)
-        await SendMessage(chat_id=self.chat.id, text=self.text)
+        await SendMessage(chat_id=self.chat.id,
+                          text=_("Привет. С помощью этого бота ты можешь "
+                                 "скачать любое видео или аудио с Ютуба."
+                                 "Для этого вам необходимо скинуть ссылку на "
+                                 "этот ролик. По всем вопросам пишите на"
+                                 "yshhenyaev@mail.ru\n"
+                                 "Для смены языка пропишите \n/language ."))
