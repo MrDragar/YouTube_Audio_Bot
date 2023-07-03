ce3dc6f61e25e237460b13e4aa93130413c957f4
MrDragar <83676864+MrDragar@users.noreply.github.com>
2023-07-04 00:52:38 +0300

    Changing language

--- bot/database/user.py
+++ bot/database/user.py
@@ -5,7 +5,7 @@ async def get_user_by_id(id: int) -> User:
     return await User.get_or_none(id=id)
 
 
-async def create_user(id, name: str, language: str) -> User:
+async def create_user(id: int, name: str, language: str) -> User:
     user = await get_user_by_id(id)
     if not user:
         user = await User.create(id=id, name=name, language=language)
@@ -17,3 +17,7 @@ async def get_users() -> list[User]:
     return await User.all()
 
 
+async def change_language(id: int, language: str) -> None:
+    user = await get_user_by_id(id)
+    user.language = language
+    await user.save()
