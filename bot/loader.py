5fa00ee82030d997b1ddf87e0800bbb8fcefc0b4
MrDragar <83676864+MrDragar@users.noreply.github.com>
2023-07-06 01:25:53 +0300

    local_server

---
+++ bot/loader.py
@@ -0,0 +1,14 @@
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.client.telegram import TelegramAPIServer
+
+from bot.config import API_TOKEN
+
+logging.basicConfig(level=logging.INFO)
+session = AiohttpSession(
+    api=TelegramAPIServer.from_base('http://localhost:8081')
+)
+bot = Bot(token=API_TOKEN, session=session)
+dp = Dispatcher()
