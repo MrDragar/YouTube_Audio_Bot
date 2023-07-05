79301bf0f264f19c0e2ce67ca6ce5d6c8861402b
MrDragar <83676864+MrDragar@users.noreply.github.com>
2023-07-05 21:07:36 +0300

    New errors

--- bot/handlers/downloaders/youtube/errors.py
+++ bot/handlers/downloaders/youtube/errors.py
@@ -3,7 +3,8 @@ import logging
 from aiogram.dispatcher.router import Router
 from aiogram.methods import SendMessage
 from aiogram.exceptions import TelegramEntityTooLarge
-
+from yt_dlp.utils import DownloadError
+from aiogram.utils.i18n import gettext as _
 
 from bot.handlers.base_handlers import StateErrorHandler
 from bot.utils.downloaders.youtube import ToBigVideo
@@ -17,10 +18,30 @@ class YoutubeErrorHandler(StateErrorHandler):
         await self.state.clear()
         if isinstance(self.event.exception, ToBigVideo) or \
                 isinstance(self.event.exception, TelegramEntityTooLarge):
-            await SendMessage(chat_id=self.event.update.message.chat.id,
-                              text="Файл весит больше 2 ГБ")
-        else:
-            logging.exception(type(self.event.exception), self.exception_name,
-                              self.exception_message)
-            # print(type(self.event.exception))
+            return SendMessage(chat_id=self.event.update.message.chat.id,
+                               text=_("Файл весит больше 2 ГБ", locale="ru"))
+        if isinstance(self.event.exception, DownloadError):
+            if "This video contains content from SME, who has blocked it in " \
+               "your country on copyright grounds" in self.event.exception.msg:
+                return SendMessage(chat_id=self.event.update.message.chat.id,
+                                   text=_("Видео заблокировано в РФ"))
+            if "Video unavailable" in self.event.exception.msg:
+                return SendMessage(chat_id=self.event.update.message.chat.id,
+                                   text=_("Видео недоступно для скачивания"))
+            if "Unable to download API page: HTTP Error 404" \
+                    in self.event.exception.msg:
+                return SendMessage(chat_id=self.event.update.message.chat.id,
+                                   text=_("Видео недоступно для скачивания."
+                                          " Ошибка 404"))
 
+            logging.exception(self.event.exception)
+            logging.info(type(self.event.exception))
+            return SendMessage(chat_id=self.event.update.message.chat.id,
+                               text=_("Произошла какая-та ошибка при"
+                                      " скачивании видео"))
+
+        logging.exception(self.event.exception)
+        logging.info(type(self.event.exception))
+        return SendMessage(chat_id=self.event.update.message.chat.id,
+                           text=_("Произошла какая-то ошибка (никто не"
+                                  " знает, из-за чего она произошла)"))
