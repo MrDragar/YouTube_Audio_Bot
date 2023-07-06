118b1a242ff9ec4b1905522df50ed7b06e5dcb70
MrDragar <83676864+MrDragar@users.noreply.github.com>
2023-07-06 20:24:21 +0300

    Removing old files

---
+++ bot/utils/services.py
@@ -0,0 +1,23 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import os
+import datetime
+
+
+async def delete_old_files():
+    directory = "video/"
+    files = os.listdir(directory)
+    for file in files:
+        file_path = os.path.join(directory, file)
+        a = os.path.getmtime(file_path)
+        time = (datetime.datetime.utcnow() - datetime.datetime.utcfromtimestamp(a)).seconds
+        try:
+            if time > 600:
+                os.remove(file_path)
+        finally:
+            ...
+
+
+def register_services():
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(delete_old_files, trigger="interval", minutes=1)
+    scheduler.start()
