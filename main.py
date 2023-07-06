6d36f725b60c8f2e1112c27f38686fb9f8ba415d
MrDragar <83676864+MrDragar@users.noreply.github.com>
2023-07-06 22:12:03 +0300

    register services

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 from bot.loader import dp, bot
 from bot.database.init_database import init_db, close_db
 from bot.middlewares import setup_i18n
+from bot.utils.services import register_services
 
 
 async def main():
@@ -10,6 +11,7 @@ async def main():
     setup_i18n(dp)
     from bot.handlers import root_router
     dp.include_router(root_router)
+    register_services()
     await dp.start_polling(bot)
     await close_db()
 
