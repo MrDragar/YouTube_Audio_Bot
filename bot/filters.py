bc17bb0bb0204bc1a315b729dcc1da583a3adef3
MrDragar <83676864+MrDragar@users.noreply.github.com>
2023-07-07 23:53:56 +0300

    Fix subscribing

--- bot/filters.py
+++ bot/filters.py
@@ -1,6 +1,6 @@
 from aiogram.filters import Filter
 from aiogram.types import Message, ChatMemberOwner, ChatMemberAdministrator, \
-    ChatMemberRestricted
+   CallbackQuery, ChatMemberMember
 from aiogram.methods import GetChatMember
 
 from bot.config import ADMINS_ID, CHANNEL_ID
@@ -15,5 +15,13 @@ class IsSubscriberFilter(Filter):
     async def __call__(self, message: Message) -> bool:
         member = await GetChatMember(chat_id=CHANNEL_ID,
                                      user_id=message.from_user.id)
-        return member in (ChatMemberRestricted, ChatMemberAdministrator,
-                          ChatMemberOwner)
+        return isinstance(member, (ChatMemberMember,
+                                   ChatMemberAdministrator, ChatMemberOwner))
+
+
+class IsSubscriberCallbackFilter(Filter):
+    async def __call__(self, callback: CallbackQuery) -> bool:
+        member = await GetChatMember(chat_id=CHANNEL_ID,
+                                     user_id=callback.from_user.id)
+        return isinstance(member, (ChatMemberMember,
+                                   ChatMemberAdministrator, ChatMemberOwner))
