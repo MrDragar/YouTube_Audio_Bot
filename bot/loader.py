af39a04f6325d3059428582d4e716e9331b57d3f
MrDragar <83676864+MrDragar@users.noreply.github.com>
2023-07-15 22:35:53 +0300

    Fix thread error

--- bot/loader.py
+++ bot/loader.py
@@ -6,7 +6,6 @@ from aiogram.client.telegram import TelegramAPIServer
 
 from bot.config import API_TOKEN
 
-logging.basicConfig(level=logging.INFO)
 session = AiohttpSession(
     api=TelegramAPIServer.from_base('http://localhost:8081'),
     timeout=0
