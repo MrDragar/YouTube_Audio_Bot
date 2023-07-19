d942656ce631522e6e304f39370583536b3b6420
MrDragar <83676864+MrDragar@users.noreply.github.com>
2023-07-19 23:12:13 +0300

    Fix advert

--- bot/handlers/advert_mixins.py
+++ bot/handlers/advert_mixins.py
@@ -10,10 +10,12 @@ from bot.database.advert import get_random_advert, add_current_number_to_advert
 class AdvertMixin(MessageHandler, ABC):
     async def send_advert(self):
         advert = await get_random_advert()
+        if not advert:
+            return
         await CopyMessage(chat_id=self.chat.id, from_chat_id=advert.chat_id,
                           message_id=advert.message_id)
         if self.from_user.id in ADMINS_ID:
-            full = add_current_number_to_advert(advert.id)
+            full = await add_current_number_to_advert(advert.id)
             if full:
                 for admin in ADMINS_ID:
                     await SendMessage(chat_id=admin,
