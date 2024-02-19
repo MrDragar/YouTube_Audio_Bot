636e107a8cb9192fad039b977231d9a1f46b2e7a
mrdragar <yshhenyaev@mail.ru>
2024-02-19 16:08:06 +0300

    run service in another thread

--- bot/utils/services.py
+++ bot/utils/services.py
@@ -1,9 +1,26 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from typing import Callable
+from functools import wraps
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import os
 import datetime
 
 
-async def delete_old_files():
+def run_async(function: Callable):
+    @wraps(function)
+    async def wrapper():
+        loop = asyncio.get_running_loop()
+        """Запускает функцию в потоке"""
+        with ThreadPoolExecutor() as pool:
+            task = loop.run_in_executor(pool, function)
+            await asyncio.gather(task)
+    return wrapper
+
+
+@run_async
+def delete_old_files():
     directory = "video/"
     files = os.listdir(directory)
     for file in files:
@@ -12,7 +29,7 @@ async def delete_old_files():
                 os.path.getmtime(file_path))
         time = (datetime.datetime.utcnow() - datetime.datetime.utcfromtimestamp(a)).seconds
         try:
-            if time > 300:
+            if time > 360:
                 os.remove(file_path)
         finally:
             ...
@@ -20,5 +37,5 @@ async def delete_old_files():
 
 def register_services():
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(delete_old_files, trigger="interval", minutes=1)
+    scheduler.add_job(delete_old_files, trigger="interval", minutes=3)
     scheduler.start()
